7ebe521a42c012a25a52512e7230b5b4fe2ff678
Tanvir ahmed (chy) <tanvirchy269@gmail.com>
2025-10-14 22:37:40 +0600

    Create strtoint.c

---
+++ Solved/strtoint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+    char x[20] = "12345";
+
+    int y = atoi(x);
+
+    printf("Int is %d\n\n",y);
+    printf("String is %s\n\n",x);
+    return 0;
+}
